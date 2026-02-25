7796f9a326feae1e5a11db4eca27416e7786a86b
ShanBirch <shannonrhysbirch@gmail.com>
2026-02-25 12:20:41 +1000

    fix: upgrade service worker cache to v2 and use network-first strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gitchat-v1';
+const CACHE_NAME = 'gitchat-v2';
 const ASSETS = [
     '/',
     '/index.html',
@@ -9,15 +9,27 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then((res) => {
-            return res || fetch(e.request);
-        })
+        fetch(e.request).catch(() => caches.match(e.request))
     );
 });
